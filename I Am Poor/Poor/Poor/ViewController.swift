ab954949d1b55ae6264a2b024fb98c512784bb7e
Aman Joshi <aman.joshi98@gmail.com>
2020-07-23 15:42:24 +0530

    Add files via upload

---
+++ I Am Poor/Poor/Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Poor
+//
+//  Created by Aman Joshi on 23/07/20.
+//  Copyright © 2020 Aman Joshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I Am Rich/Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rich
+//
+//  Created by Aman Joshi on 22/07/20.
+//  Copyright © 2020 Aman Joshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
